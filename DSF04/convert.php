8f66771d0648e34a6ba98d3968598ebf4f2be22a
Brunnofos@outlook.com <Brunnofos@outlook.com>
2024-06-11 09:55:33 -0300

    Challenge 04 - Finished | Desafio 04 - Finalizado

--- DSF04/convert.php
+++ DSF04/convert.php
@@ -14,11 +14,25 @@
             Conversor de Moedas 2.0
         </h1>
         <?php
-        $url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='06-10-2024'&@dataFinalCotacao='06-16-2024'&$top=100&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao";
+
+        $inicio = date("m-d-Y", strtotime("-7 days"));
+        $fim = date("m-d-Y");
+
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=100&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
         $dados = json_decode(file_get_contents($url), true);
 
         $cotacao = $dados["value"][0]["cotacaoCompra"];
+        $cotacaoConvertida = number_format($cotacao, 2);
+
+        $valor = $_GET["numeroEntrada"];
+        $valorConvertido = $valor/$cotacao;
+        $valorConvertidoFormatado = number_format($valorConvertido, 2);
+
+        echo "O valor de " . "<strong>$valor $</strong>" . " em reais é: <br> <strong>$valorConvertidoFormatado R$</strong>";
+        echo "<br>";
+        echo "<br>";
+        echo "A cotação atual do dólar em <strong>$fim (M/D/A)</strong> é <strong>$cotacaoConvertida $</strong>, informação extraída diretamente da API do " . '<a href="https://www.bcb.gov.br/">Banco Central do Brasil</a>';
         ?>
         <button onclick="window.history.back()">Voltar a página anterior</button>
     </main>
