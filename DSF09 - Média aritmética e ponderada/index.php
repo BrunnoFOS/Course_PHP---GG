492427a22ff7ba8db468cd0d2ea23478a7809627
BrunnoFOS <brunnofos@outlook.com>
2024-06-23 21:04:29 -0300

    Challenge 09 - Finished | Desafio 09 - Finalizado

--- DSF09 - Média aritmética e ponderada/index.php
+++ DSF09 - Média aritmética e ponderada/index.php
@@ -15,13 +15,13 @@
         </h1>
         <form action="" method="post">
             <label for="">1° nota</label>
-            <input type="number" name="n1" placeholder="Insira a 1° nota.">
+            <input type="number" name="n1" placeholder="Insira a 1° nota." required>
             <label for="">Peso - 1° nota</label>
-            <input type="number" name="p1" placeholder="Caso seja um média simples, insira 1.">
+            <input type="number" name="p1" placeholder="Caso seja um média simples, insira 1." required>
             <label for="">2° nota</label>
-            <input type="number" name="n2" placeholder="Insira a 2° nota.">
+            <input type="number" name="n2" placeholder="Insira a 2° nota." required> 
             <label for="">Peso - 2° nota</label>
-            <input type="number" name="p2" placeholder="Caso seja um média simples, insira 1.">
+            <input type="number" name="p2" placeholder="Caso seja um média simples, insira 1." required>
             <button type="submit">Calcular</button>
         </form>
     </main>
@@ -33,24 +33,31 @@
             <?php
             $num1 = $_POST['n1'] ?? 0;
             $num2 = $_POST['n2'] ?? 0;
-            $peso1 = $_POST['p1'] ?? 0.1;
-            $peso2 = $_POST['p2'] ?? 0;
+            $peso1 = $_POST['p1'] ?? 1;
+            $peso2 = $_POST['p2'] ?? 1;
             $medSimpSoma = $num1 + $num2;
-            $medSimp = $medSimpSoma / 2;
+            $medSimp = number_format($medSimpSoma / 2, 1, ".");
             $medPondSoma = $peso1 + $peso2;
-            $medPond = $medSimpSoma / $medPondSoma;
+            $medPond = number_format($medSimpSoma / $medPondSoma, 1, ".");
 
-            echo "Precisa: Formatar o número com somente 1 casa decimal e limitar as notas de 1 a 10";
-
-            if ($num1 & $num2 & $peso1 & $peso2 != 0) {
+            if  ($num1 != 0 && $num2 != 0 && $peso1 == 1 && $peso2 == 1) {
                 echo "Analisando os valores N1 = $num1, N2 = $num2. Temos que:";
                 echo"
             <ul>
-            <li>A <strong>média simples</strong> entre os valores é igual a number_format($medSimp, 1, ".")</li>;
+            <li>A <strong>média simples</strong> entre os valores é igual a $medSimp</li>
+            </ul>";
+            } elseif ($num1 != 0 && $num2 != 0 && $peso1 || $peso2 > 1) {
+                echo "Analisando os valores N1 = $num1, N2 = $num2. Temos que:";
+                echo"
+            <ul>
             <li>A <strong>média ponderada</strong> entre os valores é igual a $medPond</li>
             </ul>";
-            } 
-            ?>
+            } elseif ($num1 == 0 && $num2 == 0 && $peso1 == 1 && $peso2 == 1){
+                print('<h4>Aqui será exibido o resultado de sua média</h4>');
+            } else {
+                print('Digite valores válidos.');
+            }
+            ?> 
         </h2>
         <table>
 
