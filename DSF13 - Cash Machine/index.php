c262725d0bb39b212c608ab27bfe796f06833a8f
Brunnofos@outlook.com <Brunnofos@outlook.com>
2024-06-27 16:54:42 -0300

    Challenge 013 - ON course

---
+++ DSF13 - Cash Machine/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cash Machine </title>
+<link rel="stylesheet" href="../styles.css">
+</head>
+
+<body>
+    <main>
+        <h1>Caixa Eletrônico</h1>
+        <form action="" method="get">
+            <label for="valorEntrada">Qual valor você deseja sacar? <strong>(R$)</strong></label>
+            <input type="number" required name="valorInput">
+
+        </form>
+    </main>
+</body>
+
+</html>
